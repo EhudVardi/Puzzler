ceef3c606b9073bfa1e365138f8dcae7aab80784
EVFU-DESKTOP-WIN11\EVFUWS <not@sure>
2026-04-24 18:28:08 +0300

    modern http fetching

--- Source/Data/Handlers/WebHandler.cs
+++ Source/Data/Handlers/WebHandler.cs
@@ -1,24 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Net;
+using System.Net.Http;
 using System.IO;
 
 namespace Data
 {
     public class WebHandler
     {
+        private static readonly HttpClient _httpClient = new();
+
         private static string GetWebPage(string url)
         {
-            WebRequest request = WebRequest.Create(url);
-            WebResponse response = request.GetResponse();
-            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
-                return sr.ReadToEnd();
+            return _httpClient.GetStringAsync(url).Result;
         }
 
-        public static HtmlAgilityPack.HtmlDocument GetWebPageAsHtmlDocument(string url)
+        public static HtmlAgilityPack.HtmlDocument? GetWebPageAsHtmlDocument(string url)
         {
-            HtmlAgilityPack.HtmlDocument doc = null;
+            HtmlAgilityPack.HtmlDocument? doc = null;
 
             string data = GetWebPage(url);
             if (data != null)
